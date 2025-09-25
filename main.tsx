7cef4f554431fe0f1c3c3977cc4c0de01e8d4a47
etrobot@outlook.com <etrobot@outlook.com>
2025-09-25 11:22:10 +0800

    fix main

--- main.tsx
+++ main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { MainLayout } from './app/components/main-layout.tsx'
 import './app/globals.css'
+import React from 'react'
+// import React from 'react'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
